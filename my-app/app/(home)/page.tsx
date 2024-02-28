cd76269757319fcb1c93f960406403d754c3349d
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-02-28 19:12:32 -0300

    feat: add user table

--- my-app/app/(home)/page.tsx
+++ my-app/app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Reservation from "../_components/Reservation";
+import Header from "../_components/Header";
 
 export default function Home() {
 	return (
 		<>
+			<Header />
 			<Reservation />
 		</>
 	);
---
+++ my-app/app/data.ts
@@ -0,0 +1,36 @@
+export const heroData: {
+	pretitle: string;
+	title: string;
+	btnText: string;
+} = {
+	pretitle: "OUTSTANDING ITALIAN CUISINE",
+	title: "Botticelli",
+	btnText: "Book a table",
+};
+
+export const linksData = [
+	{
+		name: "Home",
+		slash: "/",
+	},
+	{
+		name: "About Us",
+		slash: "/about",
+	},
+	{
+		name: "Our Kitchen",
+		slash: "/Kitchen",
+	},
+	{
+		name: "Menu",
+		slash: "/menu",
+	},
+	{
+		name: "Events",
+		slash: "/events",
+	},
+	{
+		name: "Contact",
+		slash: "/contact",
+	},
+];

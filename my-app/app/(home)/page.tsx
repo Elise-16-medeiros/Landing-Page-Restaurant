8f983359f37163bae6adbd61f4ae2b443e0cadb5
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-02-28 17:19:51 -0300

    feat: download images

---
+++ my-app/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Reservation from "../_components/Reservation";
+
+export default function Home() {
+	return (
+		<>
+			<Reservation />
+		</>
+	);
+}
---
+++ my-app/app/_components/Reservation.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+const Reservation = () => {
+	return (
+		<section>
+			<Image
+				src="/bg_reservation.png"
+				width={500}
+				height={500}
+				alt="Picture of the author"
+			/>
+		</section>
+	);
+};
+
+export default Reservation;

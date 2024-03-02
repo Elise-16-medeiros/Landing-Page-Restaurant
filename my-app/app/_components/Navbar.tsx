394423e743406f07fbbf44988bb302f23342fd01
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-03-02 10:23:28 -0300

    feat: add next auth setup

---
+++ my-app/app/_components/Navbar.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { HiXMark } from "react-icons/hi2";
+import { BiMenuAltRight } from "react-icons/bi";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { linksData } from "../_lib/data";
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<nav className="bg-[#292E36] p-4">
+			<div className="container mx-auto flex items-center justify-between">
+				<div className="hidden md:flex space-x-4">
+					{linksData.map((link) => (
+						<Link className="navbar-links" href={link.slash} key={link.slash}>
+							{link.name}
+						</Link>
+					))}
+				</div>
+				<div className="text-white flex gap-3">
+					<FaFacebook className="w-[24px] h-[23px] cursor-pointer hover:text-[#E1B168]" />
+					<FaInstagram className="w-[24px] h-[23px] cursor-pointer hover:text-[#E1B168]" />
+				</div>
+
+				<div className="md:hidden">
+					<button onClick={toggleMenu} className="text-white">
+						{isOpen ? (
+							<HiXMark className="h-8 w-8 cursor-pointer" />
+						) : (
+							<BiMenuAltRight className="h-8 w-8" />
+						)}
+					</button>
+					{isOpen && (
+						<div className=" w-32 h-80 absolute gap-6 right-10 rounded-xl flex flex-col justify-center items-center bg-[#343942] transition-all duration-300">
+							{linksData.map((link) => (
+								<Link
+									className="navbar-links"
+									href={link.slash}
+									key={link.slash}
+								>
+									{link.name}
+								</Link>
+							))}
+						</div>
+					)}
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${josefin.variable} ${cormorant.variable}`}>
+			<body className={`${josefin.variable} ${cormorant.variable} bg-stone-50`}>
 				{children}
 			</body>
 		</html>

5615afab91f88b1bf90e95d9d271725ea4c28608
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-03-03 11:40:17 -0300

    feat: navbar done

--- my-app/app/_components/Navbar.tsx
+++ my-app/app/_components/Navbar.tsx
@@ -5,8 +5,19 @@ import Link from "next/link";
 import { HiXMark } from "react-icons/hi2";
 import { BiMenuAltRight } from "react-icons/bi";
 import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { IoIosLogOut, IoIosLogIn } from "react-icons/io";
+import { LuUser } from "react-icons/lu";
 import { linksData } from "../_lib/data";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+	Sheet,
+	SheetContent,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "./ui/sheet";
+import { Button } from "./ui/button";
+import { Avatar, AvatarImage } from "./ui/avatar";
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -14,13 +25,142 @@ const Navbar = () => {
 		setIsOpen(!isOpen);
 	};
 
-	const { data } = useSession();
-	const handleLoginClick = async () => {
-		await signIn();
-	};
+	const { data, status } = useSession();
+	const handleLoginClick = () => signOut();
+	const handleLogoutClick = () => signIn("google");
+
 	return (
-		<nav className="bg-[#292E36] p-4">
-			<div className="container mx-auto flex items-center justify-between">
+		<nav className="bg-[#292E36] p-4 flex justify-between items-center">
+			<div className="flex items-center gap-3">
+				{data?.user ? (
+					<>
+						{linksData.map((link) => (
+							<Link className="navbar-links" href={link.slash} key={link.slash}>
+								{link.name}
+							</Link>
+						))}
+						<Link href="/bookings" className="navbar-links">
+							Bookings
+						</Link>
+					</>
+				) : (
+					<div className="flex items-center gap-3">
+						{linksData.map((link) => (
+							<Link className="navbar-links" href={link.slash} key={link.slash}>
+								{link.name}
+							</Link>
+						))}
+					</div>
+				)}
+			</div>
+			<div>
+				<>
+					{data?.user ? (
+						<div className="flex justify-between items-center px-5 py-6">
+							<div className="flex items-center gap-3">
+								<Avatar>
+									<AvatarImage src={data.user.image ?? ""}></AvatarImage>
+								</Avatar>
+								<h2 className="font-bold text-white">{data.user.name}</h2>
+							</div>
+							<Button size="icon">
+								<IoIosLogOut
+									onClick={handleLoginClick}
+									size={20}
+									className="text-white navbar-links"
+								/>
+							</Button>
+						</div>
+					) : (
+						<>
+							<Button onClick={handleLogoutClick} className="navbar-links">
+								Login
+								<IoIosLogIn size={20} className="ml-1" />
+							</Button>
+						</>
+					)}
+				</>
+			</div>
+			<span className="md:hidden">
+				<Sheet>
+					<SheetTrigger asChild>
+						<Button size="icon" className="w-8 h-8">
+							<BiMenuAltRight size={20} className="text-white" />
+						</Button>
+					</SheetTrigger>
+					<SheetContent className="p-0">
+						<SheetHeader className="border-b border-b-[#5C6168] p-5">
+							<SheetTitle className="text-left">Menu</SheetTitle>
+						</SheetHeader>
+
+						{data?.user ? (
+							<div className="flex justify-between items-center px-5 py-6">
+								<div className="flex items-center gap-3">
+									<Avatar>
+										<AvatarImage src={data.user.image ?? ""}></AvatarImage>
+									</Avatar>
+									<h2 className="font-bold">{data.user.name}</h2>
+								</div>
+								<Button size="icon">
+									<IoIosLogOut
+										onClick={handleLoginClick}
+										size={20}
+										className="text-white"
+									/>
+								</Button>
+							</div>
+						) : (
+							<div className="flex justify-between items-center">
+								<div className="flex items-center gap-2 px-5 py-6">
+									<LuUser size={25} />
+									<h2 className="font-bold">Visitor</h2>
+								</div>
+								<Button>
+									<IoIosLogIn onClick={handleLogoutClick} size={20} />
+								</Button>
+							</div>
+						)}
+
+						<div className="flex flex-col gap-3 px-5">
+							{data?.user ? (
+								<>
+									{linksData.map((link) => (
+										<Link
+											className="navbar-links"
+											href={link.slash}
+											key={link.slash}
+										>
+											{link.name}
+										</Link>
+									))}
+									<Link href="/bookings" className="navbar-links">
+										Bookings
+									</Link>
+								</>
+							) : (
+								<div className="flex flex-col gap-3 px-5">
+									{linksData.map((link) => (
+										<Link
+											className="navbar-links"
+											href={link.slash}
+											key={link.slash}
+										>
+											{link.name}
+										</Link>
+									))}
+								</div>
+							)}
+						</div>
+					</SheetContent>
+				</Sheet>
+			</span>
+		</nav>
+	);
+};
+
+export default Navbar;
+/* 
+<div className="container mx-auto flex items-center justify-between">
 				<div className="hidden md:flex space-x-4">
 					{linksData.map((link) => (
 						<Link className="navbar-links" href={link.slash} key={link.slash}>
@@ -64,9 +204,4 @@ const Navbar = () => {
 						</div>
 					)}
 				</div>
-			</div>
-		</nav>
-	);
-};
-
-export default Navbar;
+			</div> */
